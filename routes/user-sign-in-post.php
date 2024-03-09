8c5ad9113b59bd743ae9be6d04e9010624d1e323
Ashley Gorringe <ashley@gorringe.email>
2024-03-09 18:09:48 +0000

    Development

--- routes/user-sign-in-post.php
+++ routes/user-sign-in-post.php
@@ -1,33 +1,31 @@
 <?php
 $response = (object)[];
+$response->error_fields = (object)[];
+$response->valid = true;
 
 //Checks if the email is set
-if(!isset($_POST['email'])){
-    $response->error = 'Email is not set';
-    return json_encode($response);
-    exit;
+if(empty($_POST['email'])){
+    $response->error_fields->email = 'Please enter your email address.';
+    $response->valid = false;
 }
 //Checks if the email is valid
 if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-    $response->error = 'Email is not valid';
-    return json_encode($response);
-    exit;
+    $response->error_fields->email = 'Something is wrong with your email address. Please try again.';
+    $response->valid = false;
 }
 //Checks if the email is already in use
 $email_count = $GLOBALS['database']->count('user', [
     'email' => $_POST['email']
 ]);
 if($email_count < 1){
-    $response->error = 'Email doesnt exist';
-    return json_encode($response);
-    exit;
+    $response->error_fields->email = 'This email address is not registered. Please try another one.';
+    $response->valid = false;
 }
 
 //Checks if the password is set
-if(!isset($_POST['password'])){
-    $response->error = 'Password is not set';
-    return json_encode($response);
-    exit;
+if(empty($_POST['password'])){
+    $response->error_fields->password = 'Please enter your password.';
+    $response->valid = false;
 }
 
 //get the user from the email
@@ -41,30 +39,33 @@ $user = $GLOBALS['database']->get('user',[
     'email' => $_POST['email']
 ]);
 
-//Checks if the password is correct
-if(!password_verify($_POST['password'], $user['password'])){
-    $response->error = 'Password is incorrect';
-    return json_encode($response);
-    exit;
-}else{
-    //Creates the token
-    $token = bin2hex(random_bytes(18));
-    while ($GLOBALS['database']->has('login_token', ['token' => $token])) {
+
+if($response->valid){
+    if(!password_verify($_POST['password'], $user['password'])){
+        $response->error_fields->password = 'Your password is incorrect. Please try again.';
+        $response->error = "There's something wrong with your submission. Please check the fields and try again.";
+    }else{
+        //Creates the token
         $token = bin2hex(random_bytes(18));
+        while ($GLOBALS['database']->has('login_token', ['token' => $token])) {
+            $token = bin2hex(random_bytes(18));
+        }
+
+        $GLOBALS['database']->insert('login_token', [
+            'user_id' => $user['user_id'],
+            'token' => $token
+        ]);
+
+        $response->token = $token;
+        $response->user = [
+            'user_id' => $user['user_id'],
+            'email' => $user['email'],
+            'name_full' => $user['name_full'],
+            'name_preferred' => $user['name_preferred'] ?? $user['name_full']
+        ];
     }
-
-    $GLOBALS['database']->insert('login_token', [
-        'user_id' => $user['user_id'],
-        'token' => $token
-    ]);
-
-    $response->token = $token;
-    $response->user = [
-        'user_id' => $user['user_id'],
-        'email' => $user['email'],
-        'name_full' => $user['name_full'],
-        'name_preferred' => $user['name_preferred'] ?? $user['name_full']
-    ];
-
-    return json_encode($response);
+}else{
+    $response->error = "There's something wrong with your submission. Please check the fields and try again.";
 }
+
+return json_encode($response);
--- routes/users-post.php
+++ routes/users-post.php
@@ -1,75 +1,74 @@
 <?php
 $response = (object)[];
+$response->error_fields = (object)[];
+$response->valid = true;
 
 //Checks if the email is set
 if(!isset($_POST['email'])){
-    $response->error = 'Email is not set';
-    return json_encode($response);
-    exit;
+    $response->error_fields->email = 'Please enter your email address.';
+    $response->valid = false;
 }
 //Checks if the email is valid
 if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-    $response->error = 'Email is not valid';
-    return json_encode($response);
-    exit;
+    $response->error_fields->email = 'Something is wrong with your email address. Please try again.';
+    $response->valid = false;
 }
 //Checks if the email is already in use
 $email_count = $GLOBALS['database']->count('user', [
     'email' => $_POST['email']
 ]);
 if($email_count > 0){
-    $response->error = 'Email is already in use';
-    return json_encode($response);
-    exit;
+    $response->error_fields->email = 'This email address is already in use. Please try another one.';
+    $response->valid = false;
 }
 //Checks if the password is set
-if(!isset($_POST['password'])){
-    $response->error = 'Password is not set';
-    return json_encode($response);
-    exit;
+if(empty($_POST['password'])){
+    $response->error_fields->password = 'Please enter a password.';
+    $response->valid = false;
 }
 //Checks if the password matches password_re
 if($_POST['password'] != $_POST['password_re']){
-    $response->error = 'Passwords do not match';
-    return json_encode($response);
-    exit;
+    $response->error_fields->password_re = 'The passwords do not match. Please try again.';
+    $response->valid = false;
 }
 
 //Checks if name_full is set
-if(!isset($_POST['name_full'])){
-    $response->error = 'Full name is not set';
-    return json_encode($response);
-    exit;
+if(empty($_POST['name_full'])){
+    $response->error_fields->name_full = 'Please enter your full name.';
+    $response->valid = false;
 }
 
+if($response->valid){
+    //Creates the user
+    $GLOBALS['database']->insert('user', [
+        'email' => $_POST['email'],
+        'password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
+        'name_full' => $_POST['name_full'],
+        'name_preferred' => $_POST['name_preferred'] ?? ''
+    ]);
+    $user_id = $GLOBALS['database']->id();
 
-//Creates the user
-$GLOBALS['database']->insert('user', [
-    'email' => $_POST['email'],
-    'password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
-    'name_full' => $_POST['name_full'],
-    'name_preferred' => $_POST['name_preferred'] ?? ''
-]);
-$user_id = $GLOBALS['database']->id();
-
-//Creates the token
-$token = bin2hex(random_bytes(18));
-while ($GLOBALS['database']->has('login_token', ['token' => $token])) {
+    //Creates the token
     $token = bin2hex(random_bytes(18));
+    while ($GLOBALS['database']->has('login_token', ['token' => $token])) {
+        $token = bin2hex(random_bytes(18));
+    }
+
+    $GLOBALS['database']->insert('login_token', [
+        'user_id' => $user_id,
+        'token' => $token
+    ]);
+
+    $response->token = $token;
+    $response->user = [
+        'user_id' => $user_id,
+        'email' => $_POST['email'],
+        'name_full' => $_POST['name_full'],
+        'name_preferred' => $_POST['name_preferred'] ?? $_POST['name_full']
+    ];
+}else{
+    $response->error = "There's something wrong with your submission. Please check the fields and try again.";
 }
-
-$GLOBALS['database']->insert('login_token', [
-    'user_id' => $user_id,
-    'token' => $token
-]);
-
-$response->token = $token;
-$response->user = [
-    'user_id' => $user_id,
-    'email' => $_POST['email'],
-    'name_full' => $_POST['name_full'],
-    'name_preferred' => $_POST['name_preferred'] ?? $_POST['name_full']
-];
 
 return json_encode($response);
 ?>
